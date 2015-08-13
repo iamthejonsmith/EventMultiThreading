4f56f51c9846c607f0397684b5c01925cb515f5c
Jon Smith <Wolfwood@Wolfwood.local>
2015-08-13 08:22:19 -0400

    Initial Commit

---
+++ EventMultiThreading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EventMultiThreading
+//
+//  Created by Jon Smith on 8/13/15.
+//  Copyright (c) 2015 Jon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EventMultiThreading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EventMultiThreading
+//
+//  Created by Jon Smith on 8/13/15.
+//  Copyright (c) 2015 Jon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
